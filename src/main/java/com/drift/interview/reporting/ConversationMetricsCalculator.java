011f212db1365c84483f050b95d43f46221079af
brunokoppel@gmail.com <bruno.koppel@gmail.com>
2020-04-16 16:12:05 -0400

    solutions

--- src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
+++ src/main/java/com/drift/interview/reporting/ConversationMetricsCalculator.java
@@ -14,11 +14,51 @@ public class ConversationMetricsCalculator {
   ConversationResponseMetric calculateAverageResponseTime(Conversation conversation) {
     List<Message> messages = conversation.getMessages();
 
-    // implement me!
+    long numberOfResponses = 0;
+    long firstTimeResponseFromEndUser = 0;
+    long totalResponseTime = 0;
+    long averageResponseTime = 0;
+    int i = 0;
+
+    while(messages.size() > i) {
+      if (messages.get(i).isTeamMember() == false) {
+        firstTimeResponseFromEndUser = messages.get(i).getCreatedAt();
+        while ((messages.get(i).isTeamMember() == false) && (messages.size() > i)) {
+
+          /*
+          as a little bonus I thought of showing this in the browser, but I wasn't sure because of how many files
+          I would have to change and then send along with the answer. So here it is
+
+          I was trying to find a solution with only one return while managing to get the flow control neat and simple and still have
+          the message displayed on the console. If I wasn't sure about adding the bonus because it would result in extra lines but
+          I guess I'll just leave it for test units :)
+           */
+
+          if(messages.size() - 1 == i)
+          {
+            System.out.println("Team Member never replied to End User");
+            return ConversationResponseMetric.builder()
+                    .setConversationId(conversation.getId())
+                    .setAverageResponseMs(averageResponseTime)
+                    .build();
+          }
+
+          i++;
+
+        }
+
+        totalResponseTime += (messages.get(i).getCreatedAt() - firstTimeResponseFromEndUser);
+        numberOfResponses += 1;
+        averageResponseTime = totalResponseTime / numberOfResponses;
+      }
+
+      i++;
+      }
+
 
     return ConversationResponseMetric.builder()
         .setConversationId(conversation.getId())
-        .setAverageResponseMs(0)
+        .setAverageResponseMs(averageResponseTime)
         .build();
   }
 }
